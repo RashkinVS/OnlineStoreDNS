27d333bfc889c627739ad208fc5ed3469c266096
RashkinVS <Rashckin16@yandex.ru>
2021-06-23 12:51:37 +0400

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class MainTest {
+    public static WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void openDNSMainPage() throws IOException {
+        driver.get("https://www.dns-shop.ru/");
+    }
+}
